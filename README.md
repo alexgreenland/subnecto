c4bff2e681269d42b500c80e555a84b12b45f8e6
Alex Greenland <a.greenland.6400@gmail.com>
2016-04-17 15:21:04 +0100

    Updates

--- README.md
+++ README.md
@@ -2,51 +2,6 @@
 
 A lightweight, extensible and reactive model-driven library. Model events can be subscribed to, and bubble up the hierarchy chain.
 
-# Usage
-
-Bind `subnecto` to your desired namespace through the Subnecto factory function `SubnectoFactory`.
-
-	<script src="../subnecto.js"></script>
-	<script>
-		window.Subnecto = SubnectoFactory();
-	</script>
-
-Create your models derived from the Subnecto models.
-
-	this.UserModel = function(parent) {
-		Subnecto.BaseModel.call(this, parent);
-		this.username = new Subnecto.ValueModel(this);
-	};
-
-Create a model container to hold your models, for example:
-
-    this.ModelsModel = function(parent) {
-    	Subnecto.BaseModel.call(this, parent);
-		this.user = new self.UserModel(this);
-    };
-
-Publishers are responsible for pushing changes to the models. Bind your publishers to model functions, using a DOM library of your choice.
-
-	var initModelPublishers = function() {
-
-	};
-	
-Subscribers respond to model events. Bind your subscribers to model functions, using a DOM library of your choice.
-
-	var initModelSubscribers = function() {
-		
-	};
-
-Instantiate and initialise the model. The `SuperModel` is the Subnecto model container, and is intended to be the container of your models container, `ModelsModel`.
-	    
-	var initModel = function() {
-    	var superModel = new SuperModel();
-    	superModel.models = new ModelsModel(this.superModel);
-    	initModelPublishers();
-    	initModelSubscribers();
-    };
-
-
 ## License
 
 Apache License 2.0
--- index.js
+++ index.js
@@ -24,46 +24,46 @@
 	
 		this.initModelPublishers = function() {
 			$('#username-input').keyup(function(e) {
-				self.superModel.models.user.username.update($(this).val());
+				self.modelContainer.models.user.username.update($(this).val());
 			});
             
             $('#btn-access').click(function(e) {
                 e.preventDefault();
-                self.superModel.models.user.access();
+                self.modelContainer.models.user.access();
             });
             
             $('#btn-stop-propagation').click(function(e) {
-                self.superModel.models.propagation.stop.update(true);
+                self.modelContainer.models.propagation.stop.update(true);
             });
             
             $('#btn-resume-propagation').click(function(e) {
-                self.superModel.models.propagation.stop.update(false);
+                self.modelContainer.models.propagation.stop.update(false);
             });
 		};
 	
 		this.initModelSubscribers = function() {
-			var userModel = self.superModel.models.user;
+			var userModel = self.modelContainer.models.user;
             
             userModel.on('access', function(model) {
-                $('.user-update-events').append('<code>subnectoExampleApp.superModel.models.<strong>user:access</strong></code><br>');
+                $('.user-update-events').append('<code>subnectoExampleApp.modelContainer.models.<strong>user:access</strong></code><br>');
             });
             
 			userModel.username.on('update', function(model, event) {
-                if (self.superModel.models.propagation.stop.value) {
+                if (self.modelContainer.models.propagation.stop.value) {
                     event.stopPropagation();
                 }
 				$('.username-result').html('Hello <strong>' + model.value + '</strong>!');
 			});
 			
 			userModel.username.on('update', function(model) {
-				$('.username-update-events').append('<code>subnectoExampleApp.superModel.models.<strong>user.username:update</strong></code><br>');
+				$('.username-update-events').append('<code>subnectoExampleApp.modelContainer.models.<strong>user.username:update</strong></code><br>');
 			});
 			
 			userModel.on('update', function(model) {
-				$('.user-update-events').append('<code>subnectoExampleApp.superModel.models.<strong>user:update</strong></code><br>');
+				$('.user-update-events').append('<code>subnectoExampleApp.modelContainer.models.<strong>user:update</strong></code><br>');
 			});
             
-            self.superModel.models.propagation.stop.on('update', function(model, event) {
+            self.modelContainer.models.propagation.stop.on('update', function(model, event) {
                 if (model.value) {
                     $('#btn-stop-propagation').addClass('hidden');
                     $('#btn-resume-propagation').removeClass('hidden');
@@ -82,14 +82,14 @@
 	    	// this.products = new self.ProductsModel(this);
 	    };
 		
-		this.SuperModel = function(parent) {
+		this.ModelContainer = function(parent) {
 	    	subnecto.BaseModel.call(this, parent);
-			this.models;
+			this.models = new subnecto.ValueModel();
 		};
 	
 		this.initModel = function() {
-			this.superModel = new self.SuperModel();
-			this.superModel.models = new self.ModelsModel(this.superModel);
+			this.modelContainer = new self.ModelContainer();
+			this.modelContainer.models.value = new self.ModelsModel(this.modelContainer);
 			this.initModelSubscribers();
 			this.initModelPublishers();
 		};
